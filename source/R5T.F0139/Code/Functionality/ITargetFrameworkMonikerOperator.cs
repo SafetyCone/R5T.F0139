c2251dfab6c4c62be4ebcff6fe5161cfa78c98d6
davidcoats <david@getsafetycone.com>
2023-10-18 10:46:05 -0700

    Interim changes.

--- source/R5T.F0139/Code/Functionality/ITargetFrameworkMonikerOperator.cs
+++ source/R5T.F0139/Code/Functionality/ITargetFrameworkMonikerOperator.cs
@@ -4,6 +4,8 @@ using R5T.T0132;
 using R5T.T0218;
 using R5T.T0219;
 
+using ITargetFrameworkMonikers = R5T.Z0057.Platform.ITargetFrameworkMonikers;
+
 
 namespace R5T.F0139
 {
@@ -12,10 +14,27 @@ namespace R5T.F0139
     {
         public IDotnetMajorVersion Get_DotnetMajorVersion(ITargetFrameworkMoniker targetFrameworkMoniker)
         {
-            var output = targetFrameworkMoniker.Value switch
+            // Deal with "net6.0-windows" for example.
+            var isSuffixed = Instances.StringOperator.Contains(
+                targetFrameworkMoniker.Value,
+                Instances.Characters.Dash);
+
+            var ensuredTargetFrameworkMoniker = isSuffixed
+                ? Instances.StringOperator.Get_Substring_Upto_Exclusive(
+                    Instances.StringOperator.Get_IndexOf(
+                        targetFrameworkMoniker.Value,
+                        Instances.Characters.Dash),
+                    targetFrameworkMoniker.Value)
+                : targetFrameworkMoniker.Value
+                ;
+
+            var output = ensuredTargetFrameworkMoniker switch
             {
-                Z0057.Platform.ITargetFrameworkMonikers.NET_6_Constant => Instances.DotnetMajorVersions.V6,
-                Z0057.Platform.ITargetFrameworkMonikers.Net_Standard2_1_Constant => Instances.DotnetMajorVersions.V2,
+                ITargetFrameworkMonikers.NET_5_Constant => Instances.DotnetMajorVersions.V5,
+                ITargetFrameworkMonikers.NET_6_Constant => Instances.DotnetMajorVersions.V6,
+                ITargetFrameworkMonikers.NET_App_2_2_Constant => Instances.DotnetMajorVersions.V2,
+                ITargetFrameworkMonikers.Net_Standard2_0_Constant => Instances.DotnetMajorVersions.V2,
+                ITargetFrameworkMonikers.Net_Standard2_1_Constant => Instances.DotnetMajorVersions.V2,
                 _ => throw new Exception($"Unknown target framework moniker: {targetFrameworkMoniker}")
             };
 
--- source/R5T.F0139/Code/Instances.cs
+++ source/R5T.F0139/Code/Instances.cs
@@ -5,7 +5,9 @@ namespace R5T.F0139
 {
     public static class Instances
     {
+        public static Z0000.ICharacters Characters => Z0000.Characters.Instance;
         public static Z0058.IDotnetMajorVersions DotnetMajorVersions => Z0058.DotnetMajorVersions.Instance;
+        public static L0053.IStringOperator StringOperator => L0053.StringOperator.Instance;
         public static Z0057.ITargetFrameworkMonikers TargetFrameworkMonikers => Z0057.TargetFrameworkMonikers.Instance;
     }
 }
